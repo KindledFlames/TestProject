92c565933aa1325fa9fd80134ee95bb58899e011
KindledFlames <kindledflames@gmail.com>
2017-11-07 11:30:38 -0600

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubTest
+//
+//  Created by Wilhelm, Jayden on 11/7/17.
+//  Copyright © 2017 Wilhelm, Jayden. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
